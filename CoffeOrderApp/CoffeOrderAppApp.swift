366165e0963039cfa1ebfd33e8b3ded18774b32c
Felipe Vieira Lima <felipe.lima@inter.co>
2024-07-24 20:52:18 -0300

    Initial Commit

---
+++ CoffeOrderApp/CoffeOrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeOrderAppApp.swift
+//  CoffeOrderApp
+//
+//  Created by Felipe Vieira Lima on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeOrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeOrderApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeOrderApp
+//
+//  Created by Felipe Vieira Lima on 24/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
